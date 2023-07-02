bd95ed51560a35c1c60b5268d649534944c60316
Gadest <114533276+Gadest@users.noreply.github.com>
2023-07-03 00:35:10 +0300

    completed

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,18 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        if (sourceList.size()<3) {
+            return new ArrayList<>();
+        }
+        else {
+            ArrayList<String> strings = new ArrayList<>();
+            for (int i = 0; i <= sourceList.size(); i++) {
+                if (i % 3 == 0 && i!=0) {
+                    strings.add(sourceList.get(i - 1));
+                    strings.add(sourceList.get(i - 1));
+                }
+            }
+            return strings;
+        }
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,20 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+       sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        double first = 5*Math.pow(Double.parseDouble(a),2)+3;
+        double second = 5*Math.pow(Double.parseDouble(b),2)+3;
+        if (first>second) {
+            return 1;
+        } else if (first<second) {
+            return -1;
+        }
+        else return -1;
     }
 }
